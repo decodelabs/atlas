062efe5a4d481ff9d949bbb9d872ff152b1c59a2
Tom Wright <tom@inflatablecookie.com>
2019-10-25 13:01:03 +0100

    Refactored ReceiverProxy to DataReceiver
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Atlas/DataReceiver/Proxy.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This file is part of the Atlas package
+ * @license http://opensource.org/licenses/MIT
+ */
+declare(strict_types=1);
+namespace DecodeLabs\Atlas\Channel;
+
+use DecodeLabs\Atlas\DataReceiver;
+use DecodeLabs\Atlas\DataReceiverTrait;
+
+class Proxy implements DataReceiver
+{
+    use DataReceiverTrait;
+
+    protected $receiver;
+    protected $writer;
+    protected $writable = true;
+
+    /**
+     * Init with stream path
+     */
+    public function __construct(object $receiver, callable $writer)
+    {
+        $this->receiver = $receiver;
+        $this->writer = $writer;
+    }
+
+
+    /**
+     * Set as writable
+     */
+    public function setWritable(bool $flag): Channel
+    {
+        $this->writable = $flag;
+        return $this;
+    }
+
+    /**
+     * Is the resource still writable?
+     */
+    public function isWritable(): bool
+    {
+        return $this->writable;
+    }
+
+    /**
+     * Write ?$length bytes to resource
+     */
+    public function write(?string $data, int $length=null): int
+    {
+        $this->checkWritable();
+
+        if ($data === null) {
+            return 0;
+        }
+
+        if ($length !== null) {
+            $data = substr($data, 0, $length);
+        }
+
+        ($this->writer)($this->receiver, $data);
+        return strlen($data);
+    }
+}
