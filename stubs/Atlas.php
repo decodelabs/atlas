c3db74c85bbfde9fa6ae434a3721556fd3d6241f
Tom Wright <tom@inflatablecookie.com>
2021-05-11 11:12:26 +0000

    Added Veneer IDE support stub
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ stubs/Atlas.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This is a stub file for IDE compatibility only.
+ * It should not be included in your projects.
+ */
+namespace DecodeLabs;
+use DecodeLabs\Veneer\Proxy;
+use DecodeLabs\Veneer\ProxyTrait;
+use DecodeLabs\Atlas\Context as Inst;
+class Atlas implements Proxy { use ProxyTrait; 
+const VENEER = 'Atlas';
+const VENEER_TARGET = Inst::class;
+const PLUGINS = Inst::PLUGINS;
+public static $http;};
