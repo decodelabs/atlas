b600353ec414329263584e109b0af85943e4a29c
Tom Wright <tom@inflatablecookie.com>
2022-08-24 19:10:00 +0000

    Added eclint
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- stubs/DecodeLabs/Atlas.php
+++ stubs/DecodeLabs/Atlas.php
@@ -7,7 +7,7 @@ namespace DecodeLabs;
 use DecodeLabs\Veneer\Proxy;
 use DecodeLabs\Veneer\ProxyTrait;
 use DecodeLabs\Atlas\Context as Inst;
-class Atlas implements Proxy { use ProxyTrait; 
+class Atlas implements Proxy { use ProxyTrait;
 const VENEER = 'DecodeLabs\Atlas';
 const VENEER_TARGET = Inst::class;
 const PLUGINS = Inst::PLUGINS;
